a3ab8bd3b8660eb8f6024099200927a023bc8668
alanzcc <alanzcc1@gmail.com>
2024-05-14 20:29:54 -0300

    Feat: CMake Ready and Connection Possible

--- src/run.cpp
+++ src/run.cpp
@@ -1,8 +1,49 @@
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+using namespace std;
 
-int main() {
-	std::cout << "Working so far" << std::endl;
-	return 0;
+
+string getEnvironmentVariable(const string& environmentVarKey) {
+    const char* envValue = getenv(environmentVarKey.c_str());
+    if (envValue != nullptr) {
+        return std::string(envValue);
+    } else {
+        return "";
+    }
 }
 
+vector<string> getDatabases(mongocxx::client& client)
+{
+    return client.list_database_names();
+}
 
+int main()
+{
+    // Create an instance.
+    mongocxx::instance inst{};
+    
+    // Retrieve MongoDB URI from environment variable
+    auto mongoURIStr = getEnvironmentVariable("MONGODB_URI");
+    if (mongoURIStr.empty()) {
+        cerr << "MongoDB URI not found in environment variables." << endl;
+        return 1; // Return an error code
+    }
+
+    // Create MongoDB client
+    mongocxx::uri mongoURI{ mongoURIStr };
+    mongocxx::client conn{ mongoURI };
+
+    // Get all the databases from the client
+    auto dbs = getDatabases(conn);
+    for (const auto& db : dbs)
+    {
+        cout << db << endl;
+    }
+
+    return 0;
+}
